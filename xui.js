3a5ecab96273b91d2ffa40c1c3b7924944d78735
zhomih <159560267+zhomih@users.noreply.github.com>
2024-05-09 18:34:16 +0300

    Add files via upload

---
+++ xui.js
@@ -0,0 +1,11 @@
+var score = 5000;
+var percenage = 1000;
+// простой кликер
+    function addToScore (amount) {
+        score = score + amount;
+        document.getElementById("tblc").innerHTML=score;
+        percenage = percenage - amount;
+        document.getElementById("percenage").innerHTML=percenage;
+
+    }
+    // конец
